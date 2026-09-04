d42fde17cddc2665da929105502de71defd4dda4
SrinivasaRimskyYamatov <rimsky.yamatov@gmail.com>
2026-09-04 18:48:29 +0900

    add CompilerOptions.cs

---
+++ Tiger.NET/ConpilerOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tiger.NET
+{
+    public enum OutputType
+    {
+        ConsoleApplication,
+        WindowsApplication,
+        Dll
+    }
+
+    public class CompilerOptions
+    {
+        public string SourceFilePath { get; set; } = "";
+        public string OutputFilePath { get; set; } = "output.exe";
+        public OutputType TargetType { get; set; } = OutputType.ConsoleApplication;
+        public bool SingleFile { get; set; } = false;
+
+        // .NET バージョン指定 (デフォルト: net10.0)
+        public string TargetFramework { get; set; } = "net10.0";
+    }
+}
