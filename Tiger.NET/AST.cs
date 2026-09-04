3c98d2bbb49a9e0d15f96e123eff04133b2b67f3
SrinivasaRimskyYamatov <rimsky.yamatov@gmail.com>
2026-09-04 17:29:21 +0900

    update AST.cs

--- Tiger.NET/AST.cs
+++ Tiger.NET/AST.cs
@@ -6,48 +6,48 @@ namespace Tiger.NET
 
     public class StringLiteralNode : ExpNode
     {
-        public string Value {; }
+        public string Value { get; set; }
         public StringLiteralNode(string val) => Value = val;
     }
 
     public class IntLiteralNode : ExpNode
     {
-        public int Value {; }
+        public int Value { get; set; }
         public IntLiteralNode(int val) => Value = val;
     }
 
     public class VarDeclNode : ExpNode
     {
-        public string Name {; }
-        public ExpNode Init {; }
+        public string Name { get; set; }
+        public ExpNode Init { get; set; }
         public VarDeclNode(string name, ExpNode init) { Name = name; Init = init; }
     }
 
     public class CallExpNode : ExpNode
     {
-        public string FuncName {; }
-        public List<ExpNode> Args {; }
+        public string FuncName { get; set; }
+        public List<ExpNode> Args { get; set; }
         public CallExpNode(string funcName, List<ExpNode> args) { FuncName = funcName; Args = args; }
     }
 
     public class LetExpNode : ExpNode
     {
-        public List<ExpNode> Decs {; }
-        public List<ExpNode> Body {; }
+        public List<ExpNode> Decs { get; set; }
+        public List<ExpNode> Body { get; set; }
         public LetExpNode(List<ExpNode> decs, List<ExpNode> body) { Decs = decs; Body = body; }
     }
 
     public class BinaryExpNode : ExpNode
     {
-        public string Op {; }
-        public ExpNode Left {; }
-        public ExpNode Right {; }
+        public string Op { get; set; }
+        public ExpNode Left { get; set; }
+        public ExpNode Right { get; set; }
         public BinaryExpNode(string op, ExpNode left, ExpNode right) { Op = op; Left = left; Right = right; }
     }
 
     public class VarAccessNode : ExpNode
     {
-        public string Name {; }
+        public string Name { get; set; }
         public VarAccessNode(string name) => Name = name;
     }
 }
