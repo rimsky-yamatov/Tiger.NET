16803e1e49cf8f597049ea99435adeb27e13abba
SrinivasaRimskyYamatov <rimsky.yamatov@gmail.com>
2026-09-04 18:55:35 +0900

    update CompilerOptions.cs

--- Tiger.NET/ConpilerOptions.cs
+++ Tiger.NET/ConpilerOptions.cs
@@ -2,19 +2,15 @@
 
 namespace Tiger.NET
 {
-    public enum OutputType
-    {
-        ConsoleApplication,
-        WindowsApplication,
-        Dll
-    }
-
     public class CompilerOptions
     {
         public string SourceFilePath { get; set; } = "";
         public string OutputFilePath { get; set; } = "output.exe";
         public OutputType TargetType { get; set; } = OutputType.ConsoleApplication;
-        public bool SingleFile { get; set; } = false;
+
+        // SingleFile / SelfContained プロパティを追加
+        public bool IsSingleFile { get; set; } = false;
+        public bool IsSelfContained { get; set; } = false;
 
         // .NET バージョン指定 (デフォルト: net10.0)
         public string TargetFramework { get; set; } = "net10.0";
