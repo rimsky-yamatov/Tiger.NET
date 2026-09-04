f2dd6a71fa06ac24012df75b35db0ad8265d3869
SrinivasaRimskyYamatov <rimsky.yamatov@gmail.com>
2026-09-05 07:34:16 +0900

    update CompilerOptions.cs

--- Tiger.NET/ConpilerOptions.cs
+++ Tiger.NET/ConpilerOptions.cs
@@ -2,9 +2,6 @@
 
 namespace Tiger.NET
 {
-    /// <summary>
-    /// 出力バイナリの形式を指定する列挙型
-    /// </summary>
     public enum OutputType
     {
         ConsoleApplication,
@@ -12,40 +9,23 @@ namespace Tiger.NET
         Dll
     }
 
-    /// <summary>
-    /// Tiger.NET コンパイラのオプション・設定クラス
-    /// </summary>
+    public enum OptimizationLevelKind
+    {
+        None,
+        Debug,
+        Release
+    }
+
     public class CompilerOptions
     {
-        /// <summary>
-        /// ソースコードファイルパス (.tig)
-        /// </summary>
         public string SourceFilePath { get; set; } = "";
-
-        /// <summary>
-        /// 出力ファイルパス (デフォルト: output.exe)
-        /// </summary>
         public string OutputFilePath { get; set; } = "output.exe";
-
-        /// <summary>
-        /// ターゲット出力タイプ (コンソールアプリ / Winアプリ / DLL)
-        /// </summary>
         public OutputType TargetType { get; set; } = OutputType.ConsoleApplication;
-
-        /// <summary>
-        /// 単一ファイル形式で生成するかどうか
-        /// </summary>
         public bool IsSingleFile { get; set; } = false;
-
-        /// <summary>
-        /// 自己完結型 (Self-Contained) ランタイムを含めて出力するかどうか
-        /// </summary>
         public bool IsSelfContained { get; set; } = false;
-
-        /// <summary>
-        /// ターゲットフレームワークのバージョン指定 (例: net9.0, net10.0)
-        /// デフォルト: net10.0
-        /// </summary>
         public string TargetFramework { get; set; } = "net10.0";
+        public OptimizationLevelKind OptimizationLevel { get; set; } = OptimizationLevelKind.Release;
+        public bool IncludeDebugInfo { get; set; } = false;
+        public bool VerboseOutput { get; set; } = false;
     }
 }
