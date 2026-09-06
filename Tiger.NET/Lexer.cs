7456c3b50cd90cb1c6f7662fb0856d047eddf025
SrinivasaRimskyYamatov <rimsky.yamatov@gmail.com>
2026-09-06 17:16:10 +0900

    update Lexer.cs

--- Tiger.NET/Lexer.cs
+++ Tiger.NET/Lexer.cs
@@ -11,6 +11,7 @@ namespace Tiger.NET
         End,
         Var,
         Function,
+        Struct,
         If,
         Then,
         Else,
@@ -19,26 +20,38 @@ namespace Tiger.NET
         For,
         To,
         Break,
+        Continue,
         True,
         False,
         And,
         Or,
+
         Assign,
         Plus,
         Minus,
         Multiply,
         Divide,
+        Modulo,
+
         Equal,
         NotEqual,
         LessThan,
         LessEqual,
         GreaterThan,
         GreaterEqual,
+
         LParen,
         RParen,
+        LBracket,
+        RBracket,
+        LBrace,
+        RBrace,
+
+        Dot,
         Comma,
         Colon,
         Semicolon,
+
         String,
         Int,
         Identifier,
@@ -49,11 +62,24 @@ namespace Tiger.NET
     {
         public TokenType Type { get; set; }
         public string Value { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
 
-        public Token(TokenType type, string value = "")
+        public Token(
+            TokenType type,
+            string value = "",
+            int line = 1,
+            int column = 1)
         {
             Type = type;
             Value = value;
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type}('{Value}') [{Line}:{Column}]";
         }
     }
 
@@ -61,6 +87,8 @@ namespace Tiger.NET
     {
         private readonly string _src;
         private int _pos;
+        private int _line = 1;
+        private int _column = 1;
 
         public Lexer(string src)
         {
@@ -77,289 +105,262 @@ namespace Tiger.NET
 
                 if (char.IsWhiteSpace(c))
                 {
-                    _pos++;
+                    Advance();
                     continue;
                 }
 
                 if (c == '/' && LookAhead(1) == '*')
                 {
-                    _pos += 2;
-
-                    bool closed = false;
-
-                    while (_pos < _src.Length - 1)
-                    {
-                        if (_src[_pos] == '*' &&
-                            _src[_pos + 1] == '/')
-                        {
-                            _pos += 2;
-                            closed = true;
-                            break;
-                        }
-
-                        _pos++;
-                    }
-
-                    if (!closed)
-                        throw new Exception(
-                            "Lexer Error: Unterminated comment.");
-
+                    SkipComment();
                     continue;
                 }
 
+                int line = _line;
+                int column = _column;
+
                 if (c == '"')
                 {
-                    _pos++;
-
-                    var sb = new StringBuilder();
-                    bool closed = false;
-
-                    while (_pos < _src.Length)
-                    {
-                        if (_src[_pos] == '"')
-                        {
-                            _pos++;
-                            closed = true;
-                            break;
-                        }
-
-                        if (_src[_pos] == '\\')
-                        {
-                            _pos++;
-
-                            if (_pos >= _src.Length)
-                                throw new Exception(
-                                    "Lexer Error: Unterminated string.");
-
-                            char escaped = _src[_pos];
-
-                            switch (escaped)
-                            {
-                                case 'n':
-                                    sb.Append('\n');
-                                    break;
-
-                                case 't':
-                                    sb.Append('\t');
-                                    break;
-
-                                case 'r':
-                                    sb.Append('\r');
-                                    break;
-
-                                case '\\':
-                                    sb.Append('\\');
-                                    break;
-
-                                case '"':
-                                    sb.Append('"');
-                                    break;
-
-                                default:
-                                    throw new Exception(
-                                        $"Lexer Error: Unknown escape '\\{escaped}'.");
-                            }
-
-                            _pos++;
-                            continue;
-                        }
-
-                        sb.Append(_src[_pos]);
-                        _pos++;
-                    }
-
-                    if (!closed)
-                        throw new Exception(
-                            "Lexer Error: Unterminated string.");
-
-                    tokens.Add(
-                        new Token(TokenType.String, sb.ToString()));
-
+                    tokens.Add(ReadString(line, column));
                     continue;
                 }
 
                 if (char.IsDigit(c))
                 {
-                    var sb = new StringBuilder();
-
-                    while (_pos < _src.Length &&
-                           char.IsDigit(_src[_pos]))
-                    {
-                        sb.Append(_src[_pos]);
-                        _pos++;
-                    }
-
-                    tokens.Add(
-                        new Token(TokenType.Int, sb.ToString()));
-
+                    tokens.Add(ReadNumber(line, column));
                     continue;
                 }
 
                 if (char.IsLetter(c) || c == '_')
                 {
-                    var sb = new StringBuilder();
-
-                    while (_pos < _src.Length &&
-                           (char.IsLetterOrDigit(_src[_pos]) ||
-                            _src[_pos] == '_'))
-                    {
-                        sb.Append(_src[_pos]);
-                        _pos++;
-                    }
-
-                    string value = sb.ToString();
-
-                    TokenType type = value switch
-                    {
-                        "let" => TokenType.Let,
-                        "in" => TokenType.In,
-                        "end" => TokenType.End,
-                        "var" => TokenType.Var,
-                        "function" => TokenType.Function,
-                        "if" => TokenType.If,
-                        "then" => TokenType.Then,
-                        "else" => TokenType.Else,
-                        "while" => TokenType.While,
-                        "do" => TokenType.Do,
-                        "for" => TokenType.For,
-                        "to" => TokenType.To,
-                        "break" => TokenType.Break,
-                        "true" => TokenType.True,
-                        "false" => TokenType.False,
-                        "and" => TokenType.And,
-                        "or" => TokenType.Or,
-                        _ => TokenType.Identifier
-                    };
-
-                    tokens.Add(new Token(type, value));
+                    tokens.Add(ReadIdentifier(line, column));
                     continue;
                 }
 
                 if (c == ':' && LookAhead(1) == '=')
                 {
-                    _pos += 2;
-                    tokens.Add(
-                        new Token(TokenType.Assign, ":="));
+                    Advance();
+                    Advance();
+                    tokens.Add(new Token(TokenType.Assign, ":=", line, column));
                     continue;
                 }
 
                 if (c == '<' && LookAhead(1) == '>')
                 {
-                    _pos += 2;
-                    tokens.Add(
-                        new Token(TokenType.NotEqual, "<>"));
+                    Advance();
+                    Advance();
+                    tokens.Add(new Token(TokenType.NotEqual, "<>", line, column));
                     continue;
                 }
 
                 if (c == '<' && LookAhead(1) == '=')
                 {
-                    _pos += 2;
-                    tokens.Add(
-                        new Token(TokenType.LessEqual, "<="));
+                    Advance();
+                    Advance();
+                    tokens.Add(new Token(TokenType.LessEqual, "<=", line, column));
                     continue;
                 }
 
                 if (c == '>' && LookAhead(1) == '=')
                 {
-                    _pos += 2;
-                    tokens.Add(
-                        new Token(TokenType.GreaterEqual, ">="));
+                    Advance();
+                    Advance();
+                    tokens.Add(new Token(TokenType.GreaterEqual, ">=", line, column));
                     continue;
                 }
 
-                switch (c)
+                TokenType? type = c switch
                 {
-                    case '<':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.LessThan, "<"));
-                        break;
+                    '+' => TokenType.Plus,
+                    '-' => TokenType.Minus,
+                    '*' => TokenType.Multiply,
+                    '/' => TokenType.Divide,
+                    '%' => TokenType.Modulo,
+                    '=' => TokenType.Equal,
+                    '<' => TokenType.LessThan,
+                    '>' => TokenType.GreaterThan,
+                    '(' => TokenType.LParen,
+                    ')' => TokenType.RParen,
+                    '[' => TokenType.LBracket,
+                    ']' => TokenType.RBracket,
+                    '{' => TokenType.LBrace,
+                    '}' => TokenType.RBrace,
+                    '.' => TokenType.Dot,
+                    ',' => TokenType.Comma,
+                    ':' => TokenType.Colon,
+                    ';' => TokenType.Semicolon,
+                    _ => null
+                };
 
-                    case '>':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.GreaterThan, ">"));
-                        break;
-
-                    case '=':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Equal, "="));
-                        break;
-
-                    case '+':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Plus, "+"));
-                        break;
-
-                    case '-':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Minus, "-"));
-                        break;
-
-                    case '*':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Multiply, "*"));
-                        break;
-
-                    case '/':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Divide, "/"));
-                        break;
-
-                    case '(':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.LParen, "("));
-                        break;
-
-                    case ')':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.RParen, ")"));
-                        break;
-
-                    case ':':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Colon, ":"));
-                        break;
-
-                    case ',':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Comma, ","));
-                        break;
-
-                    case ';':
-                        _pos++;
-                        tokens.Add(
-                            new Token(TokenType.Semicolon, ";"));
-                        break;
-
-                    default:
-                        throw new Exception(
-                            $"Lexer Error: Unexpected character '{c}'.");
+                if (type.HasValue)
+                {
+                    Advance();
+                    tokens.Add(new Token(type.Value, c.ToString(), line, column));
+                    continue;
                 }
+
+                throw new Exception(
+                    $"Lexer Error at {line}:{column}: unexpected character '{c}'");
             }
 
-            tokens.Add(new Token(TokenType.EOF));
-
+            tokens.Add(new Token(TokenType.EOF, "", _line, _column));
             return tokens;
+        }
+
+        private Token ReadString(int line, int column)
+        {
+            Advance();
+
+            var sb = new StringBuilder();
+
+            while (_pos < _src.Length)
+            {
+                char c = _src[_pos];
+
+                if (c == '"')
+                {
+                    Advance();
+                    return new Token(TokenType.String, sb.ToString(), line, column);
+                }
+
+                if (c == '\\')
+                {
+                    Advance();
+
+                    if (_pos >= _src.Length)
+                        break;
+
+                    char escaped = _src[_pos];
+
+                    switch (escaped)
+                    {
+                        case 'n':
+                            sb.Append('\n');
+                            break;
+                        case 't':
+                            sb.Append('\t');
+                            break;
+                        case 'r':
+                            sb.Append('\r');
+                            break;
+                        case '\\':
+                            sb.Append('\\');
+                            break;
+                        case '"':
+                            sb.Append('"');
+                            break;
+                        default:
+                            sb.Append(escaped);
+                            break;
+                    }
+
+                    Advance();
+                    continue;
+                }
+
+                sb.Append(c);
+                Advance();
+            }
+
+            throw new Exception(
+                $"Lexer Error at {line}:{column}: unterminated string literal");
+        }
+
+        private Token ReadNumber(int line, int column)
+        {
+            var sb = new StringBuilder();
+
+            while (_pos < _src.Length && char.IsDigit(_src[_pos]))
+            {
+                sb.Append(_src[_pos]);
+                Advance();
+            }
+
+            return new Token(TokenType.Int, sb.ToString(), line, column);
+        }
+
+        private Token ReadIdentifier(int line, int column)
+        {
+            var sb = new StringBuilder();
+
+            while (_pos < _src.Length &&
+                   (char.IsLetterOrDigit(_src[_pos]) || _src[_pos] == '_'))
+            {
+                sb.Append(_src[_pos]);
+                Advance();
+            }
+
+            string value = sb.ToString();
+
+            TokenType type = value switch
+            {
+                "let" => TokenType.Let,
+                "in" => TokenType.In,
+                "end" => TokenType.End,
+                "var" => TokenType.Var,
+                "function" => TokenType.Function,
+                "struct" => TokenType.Struct,
+                "if" => TokenType.If,
+                "then" => TokenType.Then,
+                "else" => TokenType.Else,
+                "while" => TokenType.While,
+                "do" => TokenType.Do,
+                "for" => TokenType.For,
+                "to" => TokenType.To,
+                "break" => TokenType.Break,
+                "continue" => TokenType.Continue,
+                "true" => TokenType.True,
+                "false" => TokenType.False,
+                "and" => TokenType.And,
+                "or" => TokenType.Or,
+                _ => TokenType.Identifier
+            };
+
+            return new Token(type, value, line, column);
+        }
+
+        private void SkipComment()
+        {
+            Advance();
+            Advance();
+
+            while (_pos < _src.Length)
+            {
+                if (_src[_pos] == '*' && LookAhead(1) == '/')
+                {
+                    Advance();
+                    Advance();
+                    return;
+                }
+
+                Advance();
+            }
+
+            throw new Exception(
+                $"Lexer Error at {_line}:{_column}: unterminated comment");
+        }
+
+        private void Advance()
+        {
+            if (_pos >= _src.Length)
+                return;
+
+            char c = _src[_pos++];
+
+            if (c == '\n')
+            {
+                _line++;
+                _column = 1;
+            }
+            else
+            {
+                _column++;
+            }
         }
 
         private char LookAhead(int offset)
         {
             int index = _pos + offset;
-
-            if (index >= 0 && index < _src.Length)
-                return _src[index];
-
-            return '\0';
+            return index < _src.Length ? _src[index] : '\0';
         }
     }
 }
